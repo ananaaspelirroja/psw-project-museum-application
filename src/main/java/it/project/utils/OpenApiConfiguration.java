8ddfb9598b3bd55238390707c4f2b6d7fc2cc64c
ananaaspelirroja <anas.martucci@gmail.com>
2024-11-08 12:25:55 +0100

    Added dependency to Swagger and added OpenApiConfiguration

---
+++ src/main/java/it/project/utils/OpenApiConfiguration.java
@@ -0,0 +1,51 @@
+package it.project.utils;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Anastasia Martucci",
+                        email = "mrtnts02t70d086q@studenti.unical.it"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - PSW project",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                clientCredentials =
+                @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/"
+                )
+        ),
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfiguration {
+}
